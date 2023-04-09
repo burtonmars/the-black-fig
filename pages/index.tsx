c3fc38192700dcc72992f452324f917f75de1c8e
UBC Student <burtonm1@students.cs.ubc.ca>
2023-04-08 17:35:02 -0700

    finish about page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,16 @@ import NavbarLanding from '@/components/navbar-landing';
 import styles from '../styles/home/Home.module.css';
 
 export default function Home() {
+
   return (
-   <div className={styles.home__container}>
+    <div className={styles.home__container}>
       <main className={styles.home__main}>
         <h1 className={styles.home__title}>
-          The Black Fig
+        The Black Fig
         </h1>
-        <div className={styles.home__navbarContainer}>
+      <div className={styles.home__navbarContainer}>
           <NavbarLanding />
-        </div>
+      </div>
       </main>
     </div>
   )
