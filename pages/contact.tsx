3e0993b1794884d268bfa9df74fa524500375bc0
UBC Student <burtonm1@students.cs.ubc.ca>
2023-04-12 15:21:19 -0700

    reservation and email forms template/styling (non functional)

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,17 +2,31 @@ import React from 'react'
 
 import styles from '../styles/contact/Contact.module.css';
 import Navbar from '@/components/navbar';
+import MapWidget from '@/components/map-widget';
+import Reservation from '@/components/reservation';
+import EmailUs from '@/components/email-us';
 
 function Contact() {
   return (
-    <div>
-        <div className='home__navbar-container'>
-            <Navbar />
+    <>
+      <div className={styles.contact__navbarContainer}>
+        <Navbar />
+      </div>
+      <div className={styles.contact__container}>
+        <div className={styles.contact__backgroundImg}>
+            <div className={styles.contact__backgroundTint}></div>
         </div>
-        <div>
-            Contact
+        <div className={styles.contact__formContainer}>
+          <Reservation />
+          <EmailUs />
         </div>
-    </div>
+        <div className={styles.contact__mapWidgetContainer_outer}>
+          <div className={styles.contact__mapWidgetContainer_inner}>
+            <MapWidget />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
