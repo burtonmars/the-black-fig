22b7d37e15fcd5229b1794676acb43e75a2ca7a3
UBC Student <burtonm1@students.cs.ubc.ca>
2023-04-16 20:52:42 -0700

    add google maps api to map-widget

--- components/map-widget.tsx
+++ components/map-widget.tsx
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Image from 'next/image';
 import { GoogleMap, useJsApiLoader, useLoadScript } from '@react-google-maps/api';
 
 import styles from '../styles/map-widget/Map-widget.module.css';
+import mapSnip from '../assets/img/maps-snip.webp';
 import locationPing from '../assets/img/location-ping.png';
 import arrow from '../assets/img/up-arrow.png';
 import phone from '../assets/img/phone.png';
 
 function MapWidget() {
+  const libraries = useMemo(() => ['places'], []);
+  const mapCenter = useMemo(
+    () => ({ lat: 49.252136, lng: -123.123434 }),
+    []
+  );
+
+  const mapOptions = useMemo<google.maps.MapOptions>(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: true,
+      scrollwheel: false,
+    }),
+    []
+  );
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyDtRsNA84RsJ0FduMH_-eoQLa7NU0Tre9k",
+    libraries: libraries as any,
+  });
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div className={styles.mapwidget__container}>
       <div className={styles.mapwidget__containerMapAndAddress}>
         <div className={styles.mapwidget__map}>
+          <GoogleMap
+            options={mapOptions}
+            zoom={13}
+            center={mapCenter}
+            mapTypeId={google.maps.MapTypeId.ROADMAP}
+            mapContainerStyle={{ width: '100%', height: '100%' }}
+            onLoad={() => console.log('Map Component Loaded...')}
+          />
         </div>
         <div className={styles.mapwidget__addressContainer}>
           <div className={styles.mapwidget__addressIcon}>
@@ -30,7 +62,7 @@ function MapWidget() {
           </div>
         </div>
       </div>
-      <a href='' className={styles.mapwidget__containerGetDirections}>
+      <a href='https://www.google.com/maps/@49.2589251,-123.1673799,15z' className={styles.mapwidget__containerGetDirections}>
         <div className={styles.mapwidget__directionsArrow}>
           <Image 
             src={arrow}
